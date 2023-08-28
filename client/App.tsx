720d3b363432fd482eee0695aa1b248e631ad156
Nitesh Manem <nacho@Nachos-MBP.fios-router.home>
2023-08-28 16:35:13 -0400

    set up boilerplate for react web app

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import MainContainer from "./Container/MainContainer";
+import NavBar from "./Container/NavBar";
 const App = () => {
   return (
     <>
-    <div>Hello</div>
-    <h1 className="flex flex-col justify-center m-80 p-20 text-3xl font-bold underline text-lime-300">Hello</h1>
+    <NavBar/>
+    <MainContainer/>
     </>
   )
 }
