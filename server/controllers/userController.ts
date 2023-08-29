3a9ad1310ef58f094c2656aa9f8261c579f7fdda
Kevin Li <kevin.li128@gmail.com>
2023-08-28 19:30:48 -0700

    authentication

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,45 +1,66 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 const bcrypt = require('bcrypt');
+const db = require('../models/jobModels');
 
 type userController = {
   createUser: RequestHandler;
   authenticateUser: RequestHandler;
 };
 
+
+/**
+ * SIGNUP: 
+ * 1. send POST request to http://localhost:3000/api/login/signup
+ * 2. if response == true, it means new data is stored in database AND user is authenticated
+ *    if response == false, it means username already exists in DB
+ * 
+ * LOGIN
+ * 1. send POST request to http://localhost:3000/api/login/
+ * 2. if response == true, user is authenticated. 
+ *    if response == false, either user does not exist or password is incorrect
+ */
+
 export const userController: userController = {
-  createUser: (req: Request, res: Response, next: NextFunction) => {
+  createUser: async (req: Request, res: Response, next: NextFunction) => {
     const { username, password } = req.body;
 
-    // const queryText = `SELECT ${username} `
-    // do query to see if username EXISTS. Set userExists = true if username exists already. 
-    const userExists = false
-    if (userExists) {
-      return res.status(200).json(false)
-    }
-    
+    const querySelectStr: string = `
+      SELECT * 
+      FROM users
+      WHERE users.username = '${username}'`;
+    const query = await db.query(querySelectStr);
+    // console.log('rows: ', query.rows)
 
-    bcrypt.hash(password, 10, (err: Error, hash: any) => {
-      // do query to insert username and hash into table
-      res.locals.loginStatus = true
-    });
+    if (query.rows.length > 0) {
+      return res.status(200).json(false);
+    }
+
+    const hash = bcrypt.hashSync(password, 10);
+    const queryInsertStr = `
+      INSERT INTO users(username, password)
+      VALUES ('${username}', '${hash}')`;
+
+    await db.query(queryInsertStr);
+    res.locals.loginStatus = true;
 
     return next();
   },
 
-  authenticateUser: (req: Request, res: Response, next: NextFunction) => {
+  authenticateUser: async (req: Request, res: Response, next: NextFunction) => {
     const { username, password } = req.body;
 
     // do query to see if username exists. if user does not exist, return false
-    const userExists = true;
-    if (!userExists) {
-      return res.status(200).json(false)
+    const queryStr = `
+      SELECT * FROM users
+      WHERE users.username = '${username}'`;
+    const query = await db.query(queryStr);
+
+    if (query.rows.length === 0) {
+      return res.status(200).json(false);
     }
 
-    // do query to get HASH corresponding to username
-    const hash = 'adsjflkaklj;adf' // placeholder. Assign hash to your hash from DB
-    bcrypt.compare(password, hash, (err: Error, result: boolean) => {
-      res.locals.loginStatus = result
-    })
+    const hash = query.rows[0].password;
+    res.locals.loginStatus = bcrypt.compareSync(password, hash);
 
     return next();
   },
--- server/routes/jobs.ts
+++ server/routes/jobs.ts
@@ -2,4 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 const express = require('express');
 const router = express.Router();
 
-const jobsController = require('../controllers/jobsController')
+import {jobsController} from '../controllers/jobsController'
+
+router.get('/', async (req: Request, res: Response) => {
+
+});
