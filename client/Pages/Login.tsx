77f70d6f80a19de55349087a24544af64eee6cd7
Anthony <avuong22699@gmail.com>
2023-08-28 20:26:15 -0400

    styling login

--- client/Pages/Login.tsx
+++ client/Pages/Login.tsx
@@ -27,12 +27,15 @@ function Login() {
   return (
     <>
     {userLogin === true ?
-    <div className='flex flex-col justify-center w-100 border-solid border-2 border-sky-500' >
-    <button onClick={()=>{setUserLogin?.(false)}}>X</button>
-    <input type='username' onChange={(e)=>{setUsername(e.target.value)}} className='bg-blue-500' placeholder='UserName Here'/>
-    <input type='password' onChange={(e)=>{setPassword(e.target.value)}} className='bg-blue-500' placeholder='Password Here'/>
-    <button type='submit' onClick={login} className='bg-blue-500 border-r-4'>Login</button>
-    HELLO
+    <div className='fixed inset-0 flex items-center justify-center z-10 backdrop-blur-sm' >
+      <div className='relative flex flex-col justify-center items-center w-1/3 h-2/5 rounded  bg-sky-500 ' >
+        <button onClick={()=>{setUserLogin?.(false)}} className='absolute top-0 right-0 p-2 rounded-2xl border-none'>X</button>
+        <div className='absolute top-0 text-3xl'>Welcome back to Git Hired!</div>
+        <input type='username' onChange={(e)=>{setUsername(e.target.value)}} className=' w-2/5 rounded m-2' placeholder='UserName Here'/>
+        <input type='password' onChange={(e)=>{setPassword(e.target.value)}} className=' w-2/5 rounded m-2' placeholder='Password Here'/>
+        <button type='submit' onClick={login} className='bg-blue-500 w-2/5 rounded' >Login</button>
+        <div>Not a User? Sign up <a href='/createuser'>here</a></div>
+    </div>
     </div>
      : " "}
     </>
