f2a986fe35926e6302c5f1b20f2ad512b83f2eeb
Tommy Huynh <tommysmacbook@tommys-mbp.lan>
2023-08-28 23:57:59 -0500

    added get function to jobs endpoint

--- server/routes/jobs.ts
+++ server/routes/jobs.ts
@@ -1,9 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
-const express = require('express');
+import express from 'express';
 const router = express.Router();
-
+const db = require('../models/jobModels');
 import {jobsController} from '../controllers/jobsController'
 
-router.get('/', async (req: Request, res: Response) => {
-
+router.get('/:userId', async (req: Request, res: Response) => {
+  //Getting the user id through the URL
+  const userId = req.params.userId;
+  const queryStr: string = `
+  SELECT *
+  FROM applications
+  WHERE applications.user_id = ${userId}`;
+  const query = await db.query(queryStr);
+  return res.status(200).json(query);
 });
+
+module.exports = router;
