d63d5f5431f680541fa56bd1ff347f86b7a22ba2
Nitesh Manem <nacho@Nachos-MBP.fios-router.home>
2023-08-29 20:44:22 -0400

    fixed bug

--- client/globalFunction.tsx
+++ client/globalFunction.tsx
@@ -3,6 +3,11 @@ export function useridFromCookie() {
   const allCookies = document.cookie
     .split(';')
     .map((cookie) => cookie.trim());
-  return Number(allCookies[2].split('=')[1]);
+  for(let i = 0; i < allCookies.length; i++){
+    if(allCookies[i].startsWith('user_id=')) {
+      return Number(allCookies[i].split('=')[1]);
+    }
+  }
+  return -1;
 
 }
