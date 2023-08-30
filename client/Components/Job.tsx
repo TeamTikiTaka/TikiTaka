9a2ca7df66da1ad2aee0b3f87ad68eb4ce300225
Nitesh Manem <nacho@Nachos-MBP.fios-router.home>
2023-08-29 22:15:16 -0400

    stash

--- client/Components/Job.tsx
+++ client/Components/Job.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { JobData } from '../../types/types';
 
-function Job({job, setEditForm, setShowModal} : { job: JobData, setEditForm(value: JobData): void, setShowModal(value: boolean): void}) {
+function Job({ job, setEditForm, setShowModal }: { job: JobData, setEditForm(value: JobData): void, setShowModal(value: boolean): void }) {
   return (
     <div className="grid grid-cols-9 gap-4 p-4 border-b text-white">
       <div className="flex justify-center">{job.company}</div>
@@ -13,21 +13,22 @@ function Job({job, setEditForm, setShowModal} : { job: JobData, setEditForm(valu
       </div>
       <div className="flex justify-center">{job.status}</div>
       <div className="flex justify-center">{job.notes}</div>
-      <button 
-          className="w-2/3 py-2 mt-4 bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring focus:ring-blue-300"
-          onClick={() => {
-            setEditForm(job)
-            setShowModal(true)
-          }}
-        >
-          Edit
-        </button>
-        <button 
-          className="w-2/3 py-2 mt-4 bg-red-500 hover:bg-red-700 focus:ring focus:ring-red-300"
-          onClick={() => setShowModal(true)}
-        >
-          Delete
-        </button>
+      <button
+        className="w-2/3 py-2 mt-4 bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring focus:ring-blue-300"
+        onClick={() => {
+          setEditForm(job)
+          setShowModal(true)
+
+        }}
+      >
+        Edit
+      </button>
+      <button
+        className="w-2/3 py-2 mt-4 bg-red-500 hover:bg-red-700 focus:ring focus:ring-red-300"
+        onClick={() => setShowModal(true)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
